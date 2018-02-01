ecbdaeb68de46e036b3b9cba9f6ed5aef1604ba9
Kevin Harrington <mad.hephaestus@gmail.com>
2018-02-01 11:16:48 -0500

    bevel gear generator working with parametrics

--- bevelGear.groovy
+++ bevelGear.groovy
@@ -2,17 +2,21 @@
 
 
 
-List<Object>  makeGear(int numTeeth,def thickness){
-	double toothAngle = (360.0)/24.0
-	double toothBaseArchLen = (26.15/2)*toothAngle*Math.PI/180
-	
+List<Object>  makeGear(double numTeeth,double thickness,double ratio){
+	double toothAngle = (360.0)/numTeeth
+	double toothBaseArchLen = (26.15/2)*((360.0)/24)*Math.PI/180
 	double baseDiam = (toothBaseArchLen*numTeeth)/Math.PI
-	double topDiam = baseDiam-(thickness*2)
+	double otherTeeth = (1.0/ratio)*numTeeth
+	double bevelAngle = Math.toDegrees(Math.atan2(otherTeeth,numTeeth))
+
+	double topDiamOffset = thickness/(Math.tan(Math.toRadians(bevelAngle)))
+	double topDiam = baseDiam-topDiamOffset*2
 	double baseThickness = toothBaseArchLen/Math.PI
 	double baseDiamLower = baseDiam-(baseThickness*2)
 	double totalThickness = thickness+baseThickness
-	double toothDepth = baseThickness*2
-	println "Tooth Angle " +toothAngle+" tooth baseArchLen "+toothBaseArchLen+" base Diam "+ baseDiam+" top diam "+topDiam
+	double toothDepth = baseThickness*1.5
+	//println "Tooth Angle " +toothAngle+" tooth baseArchLen "+toothBaseArchLen+" base Diam "+ baseDiam+" top diam "+topDiam+" thickness "+thickness+" toothInset "+topDiamOffset
+	println "Tooth num " +numTeeth+" other "+otherTeeth+" angle "+bevelAngle
 	CSG upperSection =new Cylinder(baseDiam/2,topDiam/2,thickness,(int)numTeeth).toCSG() // a one line Cylinder
 	CSG lowerSection =new Cylinder(baseDiamLower/2,baseDiam/2,baseThickness,(int)numTeeth).toCSG() // a one line Cylinder
 					.toZMax()
@@ -24,10 +28,12 @@ List<Object>  makeGear(int numTeeth,def thickness){
 					.rotz(45)
 					.scaley(0.5)
 					.toZMax()
-					//.movez(thickness)
-					.roty(45)
 					.movez(totalThickness)
-					.movex(topDiam/2-toothDepth)
+					.movex(-toothDepth)
+					//.movez(thickness)
+					.roty(90-bevelAngle)
+					.movez(totalThickness)
+					.movex(topDiam/2)
 	toothCutter = toothCutter.rotz(-toothAngle/8).union(toothCutter.rotz(toothAngle/8)).hull()
 	for(int i=0;i<numTeeth;i++){
 		blank=blank.difference(toothCutter.rotz(toothAngle*i))
@@ -36,20 +42,20 @@ List<Object>  makeGear(int numTeeth,def thickness){
 	return [blank,baseDiam/2,toothAngle]
 }
 
-List<Object> makeBevelBox(int numDriveTeeth, int numDrivenTeeth,Number thickness){
-	def gearA = makeGear(numDriveTeeth,thickness)
-	def gearB = makeGear(numDrivenTeeth,thickness)
+List<Object> makeBevelBox(double numDriveTeeth, double numDrivenTeeth,Number thickness){
+	
+	def gearA = makeGear(numDriveTeeth,thickness,numDriveTeeth/numDrivenTeeth)
+	def gearB = makeGear(numDrivenTeeth,thickness,numDrivenTeeth/numDriveTeeth)
 	double aDiam = gearA.get(1)
 	double bDiam = gearB.get(1)
-	double bangle = gearB.get(2)
+	double bangle = gearA.get(2)
 	CSG gearBFinal = gearB.get(0)
-					.rotz(bangle/2)
-					.rotx(-90)
-					.movey(aDiam)
+					.roty(90)
+					.movex(aDiam)
 					.movez(bDiam)
 	
-	return [gearA.get(0),gearBFinal]
+	return [gearA.get(0).rotz(bangle/2),gearBFinal]
 }
 
 
-return makeBevelBox(24,24,4.45)
+return makeBevelBox(40,12,4.45)
