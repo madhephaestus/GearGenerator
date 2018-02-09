63b326edc579984c3111281fccd13bcf9a24f9f1
Kevin Harrington <mad.hephaestus@gmail.com>
2018-02-09 16:49:54 -0500

    ratio properly calculated

--- gearBoxGenerator.groovy
+++ gearBoxGenerator.groovy
@@ -6,10 +6,10 @@ if(args == null){
 	args = [	new Cylinder(10,40,(int)6).toCSG(), // input shaft
 			new Cylinder(10,40,(int)6).toCSG(), // output shaft
 			Vitamins.get("ballBearing","R8-60355K505"),// bearing CSG
-			computeGearPitch(26.15,24),// gear pitch in arc length mm
+			computeGearPitch(26.15,24)*2,// gear pitch in arc length mm
 			90,// output shaft angle
-			3.0,// gear ratio
-			4,// number of stages
+			3,// gear ratio
+			1,// number of stages
 			true// extend shaft out the back
 			]
 }
@@ -23,11 +23,29 @@ CSG bearing = args.get(2)
 CSG inShaft = args.get(0)
 CSG outShaft = args.get(2)
 
-double ratioPerStage = ratio/numberOfStages
-int numTeeth =(int)((bearing.getTotalX()+pitch*2+ bearing.getTotalZ()*2)*Math.PI/pitch)+1
-int numTeethSecond = numTeeth*ratioPerStage
+double ratioPerStage
+int additionalTeeth
+int numTeeth
+int numTeethSecond
 
-println "Gear set "+numTeeth+" to "+numTeethSecond
+while(true ){
+	ratioPerStage =((ratio)/numberOfStages)
+	if(ratioPerStage>2){
+		numberOfStages++
+		continue
+	}else
+		break
+}
+
+numTeeth =(int)((bearing.getTotalX()+pitch*2+ bearing.getTotalZ()*2)*Math.PI/pitch)
+if(numTeeth%2!=0){
+	numTeeth+=1
+}
+
+additionalTeeth = ((double)numTeeth)*ratioPerStage
+numTeethSecond = additionalTeeth
+
+println ratioPerStage+" ratio on each "+numberOfStages+" Gear sets "+numTeeth+" to "+additionalTeeth
 
 def mainGears = ScriptingEngine.gitScriptRun(
             "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
@@ -35,7 +53,7 @@ def mainGears = ScriptingEngine.gitScriptRun(
             // Parameters passed to the funcetion
             [	  numTeeth,// Number of teeth gear a
 	            numTeethSecond,// Number of teeth gear b
-	            3.5,// thickness of gear A
+	            6,// thickness of gear A
 	           pitch,// gear pitch in arc length mm
 	           shaftAngle,// shaft angle, can be from 0 to 100 degrees
 	            0// helical angle, only used for 0 degree bevels
